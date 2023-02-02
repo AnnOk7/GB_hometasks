0834905cc34ec0831df532637622c7dac1ea5572
Anna Ok <ann.ok797@gmail.com>
2023-02-02 21:22:01 +0300

    Решила task 15

---
+++ Домашнее задание/Знакомство с языками программирования/29.01 С#/Task_15/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+Console.Clear();
+
+string[] The_days_of_the_week = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+
+Console.WriteLine("Введите цифру, обозначающую день недели:");
+int Num = int.Parse(Console.ReadLine());
+
+int Day = Num - 1;
+
+if (Day < The_days_of_the_week.Length)
+{
+    if (Num <= 5)
+    {
+        Console.WriteLine($"Посмотри внимательнее, это же {The_days_of_the_week[Day]} и это рабочий день");
+    }
+    else
+    {
+        Console.WriteLine("Это выходной день");
+    }
+}
+else 
+{
+   Console.WriteLine("Такого дня недели не существует"); 
+}
