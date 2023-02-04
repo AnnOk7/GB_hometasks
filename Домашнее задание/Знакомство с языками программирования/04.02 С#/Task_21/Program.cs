073dc7d0abae2971dfde2ac594850caa758622f4
Anna Ok <ann.ok797@gmail.com>
2023-02-04 17:15:09 +0300

    Решила все задачи Семинара 3

---
+++ Домашнее задание/Знакомство с языками программирования/04.02 С#/Task_21/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Write("Введите X1: ");
+int X1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Y1: ");
+int Y1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Z1: ");
+int Z1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите X2: ");
+int X2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Y2: ");
+int Y2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Z2: ");
+int Z2 = int.Parse(Console.ReadLine()!);
+
+
+double X = Math.Abs(X1 - X2);
+double Y = Math.Abs(Y1 - Y2);
+double Z = Math.Abs(Z1 - Z2);
+
+double XX = Math.Pow(X, 2);
+double YY = Math.Pow(Y, 2);
+double ZZ = Math.Pow(Z, 2);
+
+double result = Math.Sqrt(XX + YY + ZZ);
+result = Math.Round(result, 2);
+
+Console.WriteLine($"Расстояние между точками: {result}");
