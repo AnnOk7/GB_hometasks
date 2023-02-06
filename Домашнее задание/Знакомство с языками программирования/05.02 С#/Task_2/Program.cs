c36b497f3b40f478e9d5e75afddf69e6367bab12
Anna Ok <ann.ok797@gmail.com>
2023-02-06 19:52:50 +0300

    решила все задачи из семинара 05.02

---
+++ Домашнее задание/Знакомство с языками программирования/05.02 С#/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+
+for (int i = 1; i <= num; i++)
+{
+    factorial *= i;
+    if (i == num)
+    {
+        Console.Write("{0}", i);
+    }
+    else
+    {
+        Console.Write("{0} * ", i);
+    }
+}
+Console.Write(" = {0}", factorial);
