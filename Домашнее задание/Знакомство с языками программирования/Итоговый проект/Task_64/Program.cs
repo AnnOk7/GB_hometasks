da7eb12426c032ccae7e5998e2bc5385c5c218fc
Anna Ok <ann.ok797@gmail.com>
2023-02-26 20:26:08 +0300

    Итоговый проект

---
+++ Домашнее задание/Знакомство с языками программирования/Итоговый проект/Task_64/Program.cs
@@ -0,0 +1,16 @@
+﻿// // Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+Console.Clear();
+string Recursion(int N)
+{
+    if (N == 1) return N.ToString();
+    else
+    {
+        return N.ToString()+", " + Recursion(N - 1);
+    }
+}
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Натуральные числа до введенного числа включительно в обратную сторону: {Recursion(N)}");
---
+++ Домашнее задание/Знакомство с языками программирования/Итоговый проект/Task_66/Program.cs
@@ -0,0 +1,22 @@
+﻿// // Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+int Recursion(int N, int M)
+{
+    int sum = 0;
+    if (N == M) return N;
+    else
+    {
+        return sum = N + Recursion(N + 1, M);
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите число от: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число до: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.WriteLine(Recursion(N, M));
